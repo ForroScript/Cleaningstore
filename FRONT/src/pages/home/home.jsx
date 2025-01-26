22f0b436f57b433866e34af2562b445905df4956
Gilson1995 <gilsoncs2016@gmail.com>
2025-01-26 18:54:43 -0300

    inicio da criacao da pagina login

--- FRONT/src/pages/home/home.jsx
+++ FRONT/src/pages/home/home.jsx
@@ -1,6 +1,6 @@
-import "./home.css"
+import "./Home.css"
 import HeaderOne from "../../components/header/headerOne/headerOne"
-import Footer from "../../components/footer/footer"
+import Footer from "../../components/footer/Footer"
 
 export default function home() {
     return(
---
+++ FRONT/src/pages/login/Login.jsx
@@ -0,0 +1,10 @@
+import style from "./Login.module.css"
+
+export default function Login() {
+    return(
+        <>
+        <h1 className={style.titulo}>hello world</h1>
+        <h1 id={style.titulo}>essa é a pagina de login</h1>
+        </>
+    )
+}
